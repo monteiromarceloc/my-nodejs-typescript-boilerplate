73457dce039db62f86bded86e762e28ddabc590a
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-04-22 15:51:02 -0300

    config eslint

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ status: 'ok'})
-})
+app.get('/', (request, response) => response.json({ status: 'ok' }));
 
 app.listen(3333, () => {
-  console.log(' Server started at 3333')
+  console.log('Server started at 3333');
 });
