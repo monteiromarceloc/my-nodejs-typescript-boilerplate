87d8a43b8da3e86ce4992c6eb33ddc7cd1012e2e
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-04-22 15:57:13 -0300

    config prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
+import routes from "./routes";
 
 const app = express();
-
-app.get('/', (request, response) => response.json({ status: 'ok' }));
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server started at 3333');
+  console.log("Server started at 3333");
 });
